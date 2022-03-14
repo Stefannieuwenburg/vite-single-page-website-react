25c674eef43a1010b2d56c95c451077faa035aea
Stefan Nieuwenburg <nieuwenburg.stefan@gmail.com>
2022-03-14 18:45:51 +0100

    commit

--- src/component/List.jsx
+++ src/component/List.jsx
@@ -1,5 +1,5 @@
-import {  Link } from "react-router-dom";
-import logo from '../images/logo.png';
+import { Link } from "react-router-dom";
+import logo from "../images/logo.png";
 
 export default function List() {
     return (
